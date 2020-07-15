52976f9cfdb96de8c16bc96402f8e289e9f41fb9
Maysoon Tabali <mtab03@me.com>
2020-07-15 21:44:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,7 @@ end
 def introduction_with_language(name, language)
   puts "Hi, may name is #{name} and I am learning to program in #{language}."
 end 
+introduction_with_language(Dan, Ember.js)
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."  
